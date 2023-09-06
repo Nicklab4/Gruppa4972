c31736cfb35775d2e32bfca00aaaf349501e3869
Nicklab4 <108210847+Nicklab4@users.noreply.github.com>
2023-09-07 03:21:08 +0500

    Добавлены решения заданий 05 и 07

--- Sem1Task5/Program.cs
+++ Sem1Task5/Program.cs
@@ -2,5 +2,49 @@
 // Напишите программу, которая на вход принимает одно число (N),
 // а на выходе показывает все целые числа в промежутке от -N до N.
 
-int day = int.Parse(Console.ReadLine()); //?? "0";
 
+////////////////////////////////
+// Вариант решения 1
+////////////////////////////////
+
+
+int a = int.Parse(Console.ReadLine()); //?? "0";
+
+for (int i = -a; i <= a; i++)
+{
+    if (i == a)
+        System.Console.Write(i);
+    else
+        System.Console.Write(i + ", ");
+}
+
+////////////////////////////////
+// Вариант решения 2
+////////////////////////////////
+
+System.Console.WriteLine();
+
+int n = int.Parse(Console.ReadLine());
+int minusN = -n;
+
+for (int i = minusN; i < n; i++)
+{
+    System.Console.Write(i.ToString() + ", ");
+}
+System.Console.Write(n.ToString());
+
+////////////////////////////////
+// Вариант решения 3
+////////////////////////////////
+
+System.Console.WriteLine();
+
+int numN = int.Parse(Console.ReadLine());
+int evNum = (-1) * numN;
+
+while (evNum < numN)
+{
+    System.Console.Write(evNum + ", ");
+    evNum++;
+}
+System.Console.Write(numN);
